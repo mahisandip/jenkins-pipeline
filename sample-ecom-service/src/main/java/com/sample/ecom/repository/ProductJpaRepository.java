ae68a405a4bebc9962785907cc88f986720ddc17
mahisandip <38370380+mahisandip@users.noreply.github.com>
2024-07-22 15:15:10 +0800

    Delete sample-ecom-service directory

--- sample-ecom-service/src/main/java/com/sample/ecom/repository/ProductJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sample.ecom.jpa.ProductEntity;
-
-public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
-
-}
